960e143d07ee14def7e7e29ab272f0a6ee794f21
haifazahirahr <haifazahirahr5@gmail.com>
2024-03-20 17:06:48 +0700

    modify

--- kuliah/pertemuan6/latihan1.php
+++ kuliah/pertemuan6/latihan1.php
@@ -1,3 +1,24 @@
+<?php
+// Array 
+// Membuat array
+$hari = array("senin", "selasa", "rabu");
+$bulan = ["januari", "februari", "maret"];
+$arr = [100, "teks", true];
+
+//menampilkan array
+//versi debuging
+var_dump($hari);
+echo "<br>";
+print_r($bulan);
+
+//menampilkan 1 elemen pada array
+echo $arr[0];
+echo "<br>";
+echo "<br>";
+?>
+
+<!-- array yang menggunakan pengulangan -->
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,7 +33,7 @@
             background-color: skyblue;
             text-align: center;
             line-height: 30px;
-            margin: 30px;
+            margin: 3px;
             float: left;
             transition: 1s;
         }
@@ -21,18 +42,27 @@
             transform: rotate(360deg);
             border-radius: 50%;
         }
+
+        .clear {
+            clear: both;
+        }
     </style>
 </head>
 
 <body>
     <?php
-    $angka = [[1, 2, 3,], [4, 5, 6], [7, 8, 9]];
+    $angka = [
+        [1, 2, 3,],
+        [4, 5, 6],
+        [7, 8, 9]
+    ];
     ?>
 
     <?php foreach ($angka as $a) : ?>
         <?php foreach ($a as $b) : ?>
             <div class="kotak"><?= $b; ?></div>
         <?php endforeach; ?>
+        <div class="clear"></div>
     <?php endforeach; ?>
 </body>
 
